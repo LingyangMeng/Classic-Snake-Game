3166970938a577663f831cc0c73c3c671dc66a14
Leo Meng <menglingyang0102@gmail.com>
2023-01-09 11:25:52 +0000

    create a starting position by creating a tuple

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Classic Snake Game")
 
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+
+
 segment_1 = Turtle("square")
 segment_1.color("white")
 
