b0b134660d8da5543146c9f05c0d0f72d9e3a5bb
Leo Meng <menglingyang0102@gmail.com>
2023-01-09 11:22:42 +0000

    let the second segment go to a certain location

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ segment_1.color("white")
 
 segment_2 = Turtle("square")
 segment_2.color("white")
+segment_2.goto(-20, 0)
 
 segment_3 = Turtle("square")
 segment_3.color("white")
+
 
 screen.exitonclick()
