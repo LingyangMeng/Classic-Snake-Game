f5feebf511ef77fc0f347235aecfc186f27a6a81
Leo Meng <menglingyang0102@gmail.com>
2023-01-11 03:12:26 +0000

    loop the position inside the tuple

--- main.py
+++ main.py
@@ -9,15 +9,9 @@ starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
 for position in starting_positions:
 
-segment_1 = Turtle("square")
-segment_1.color("white")
+    segment = Turtle("square")
+    segment.color("white")
+    segment.goto(position)
 
-segment_2 = Turtle("square")
-segment_2.color("white")
-segment_2.goto(-20, 0)
-
-segment_3 = Turtle("square")
-segment_3.color("white")
-segment_3.goto(-40, 0)
 
 screen.exitonclick()
